ebb169fdb41956a8881fd26ea3aeb309fb191e9c
ruona <ogheneruonaagadagba4@gmail.com>
2021-08-31 21:19:28 +0100

    added the list of planets to the frontend

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,5 +1,6 @@
-const parse = require("csv-parse");
 const fs = require("fs");
+const path = require("path");
+const parse = require("csv-parse");
 
 const habitablePlantes = [];
 
@@ -12,30 +13,34 @@ const isHabitablePlanet = (planet) => {
   );
 }; //this function will filter out those plantes which are habitable
 
-fs.createReadStream("kepler_data.csv") //this enable us to open up a readable stream
-  .pipe(
-    parse({
-      comment: "#",
-      columns: true,
-    })
-  )
-  .on("data", (data) => {
-    if (isHabitablePlanet(data)) {
-      habitablePlantes.push(data);
-    }
-  }) //this is used to read file
-  .on("err", (err) => {
-    console.log(err);
-  })
-  .on("end", () => {
-    console.log(
-      habitablePlantes.map((planet) => {
-        return planet["kepler_name"];
+const loadPlanetsData = () => {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    ) //this enable us to open up a readable stream
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlantes.push(data);
+        }
+      }) //this is used to read file
+      .on("err", (err) => {
+        console.log(err);
+        reject(err);
       })
-    );
-    console.log(`${habitablePlantes.length} habitable plantes found`);
+      .on("end", () => {
+        console.log(`${habitablePlantes.length} habitable plantes found`);
+        resolve();
+      });
   });
+};
 
 module.exports = {
+  loadPlanetsData,
   planets: habitablePlantes,
 };
