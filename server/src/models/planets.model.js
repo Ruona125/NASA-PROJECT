679fa0597728905d74d946b1dec85fc97eec86d8
ruona <ogheneruonaagadagba4@gmail.com>
2021-08-31 20:39:10 +0100

    added a csv file to the project

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,41 @@
-const planets = [];
+const parse = require("csv-parse");
+const fs = require("fs");
 
-module.exports = planets;
+const habitablePlantes = [];
+
+const isHabitablePlanet = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}; //this function will filter out those plantes which are habitable
+
+fs.createReadStream("kepler_data.csv") //this enable us to open up a readable stream
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlantes.push(data);
+    }
+  }) //this is used to read file
+  .on("err", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(
+      habitablePlantes.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
+    console.log(`${habitablePlantes.length} habitable plantes found`);
+  });
+
+module.exports = {
+  planets: habitablePlantes,
+};
