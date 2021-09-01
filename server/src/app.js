53b614cca10ce3d2e642300f39741c08582980f8
ruona <ogheneruonaagadagba4@gmail.com>
2021-09-01 23:24:54 +0100

    fixed the routing bug

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const planetsRouter = require("./routes/plantes/planets.router");
-
+const launchesRouter = require("./routes/launches/launches.router");
 const app = express();
 
 app.use(
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(planetsRouter);
-app.get("/", (req, res) => {
+app.use(launchesRouter);
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
