510b5cb68dbb0f5ca3ef168b85f5b96d7e47e661
ruona <ogheneruonaagadagba4@gmail.com>
2021-09-09 11:55:06 +0100

    added shema to the launches model

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,32 @@
 const http = require("http");
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  "mongodb+srv://nasa-api:ub55c5LgqigvzMKy@nasacluster.unoaa.mongodb.net/nasa?retryWrites=true&w=majority";
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 const startServer = async () => {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTypology: true,
+  });
   await loadPlanetsData();
 
   server.listen(PORT, () => {
