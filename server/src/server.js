662e1bd72b9b4b483b3453d762cbcb750332321c
ruona <ogheneruonaagadagba4@gmail.com>
2021-09-14 09:44:38 +0100

    tested the mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,24 +1,16 @@
 const http = require("http");
-const mongoose = require("mongoose");
+
+// const mongoose = require("mongoose");
 
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
+const { mongoConnect } = require("./services/mongo");
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL =
-  "mongodb+srv://nasa-api:ub55c5LgqigvzMKy@nasacluster.unoaa.mongodb.net/nasa?retryWrites=true&w=majority";
-
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
 // await mongoose.connect(MONGO_URL, {
 //   useNewUrlParser: true,
 //   // useFindAndModify: false,
@@ -33,7 +25,9 @@ async function startServer() {
   //   // useCreateIndex: true,
   //   useUnifiedTopology: true,
   // });
-  await mongoose.connect(MONGO_URL);
+
+  // await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
