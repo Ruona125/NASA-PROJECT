624b2043e84448c3508706bc1ae2a503931214d2
ruona <ogheneruonaagadagba4@gmail.com>
2021-09-14 13:42:14 +0100

    mapped spaceX data to our database

--- server/src/server.js
+++ server/src/server.js
@@ -6,29 +6,16 @@ const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
 const { mongoConnect } = require("./services/mongo");
+const { loadLaunchData } = require("./models/launches.model");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-// await mongoose.connect(MONGO_URL, {
-//   useNewUrlParser: true,
-//   // useFindAndModify: false,
-//   // useCreateIndex: true,
-//   useUnifiedTopology: true,
-// });
-
 async function startServer() {
-  // await mongoose.connect(MONGO_URL, {
-  //   useNewUrlParser: true,
-  //   // useFindAndModify: false,
-  //   // useCreateIndex: true,
-  //   useUnifiedTopology: true,
-  // });
-
-  // await mongoose.connect(MONGO_URL);
   await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
 
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
